dcc9decddad72927c38c17ba378e1a099a876389
dinah-parag <dinah.dantass@gmail.com>
2026-01-06 14:41:36 -0300

    estrutura e dependencias

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Cart() {
+  return (
+    <div>
+      <h1>Meu Carrinho</h1>
+      <p>Seus itens aparecerão aqui.</p>
+    </div>
+  );
+}
+
+export default Cart;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Vitrine de Produtos</h1>
+      <p>Aqui ficarão nossos produtos.</p>
+    </div>
+  );
+}
+
+export default Home;
